089248d2748256c78a1617fc8d15df8ae7470ddc
Cankat Abacı <cankatabaci95@gmail.com>
2018-09-22 17:20:09 +0300

    Add RPC Publisher and RPC Consumer

---
+++ DotnetCoreRabbitMQSamples/RpcConsumer/Program.cs
@@ -0,0 +1,90 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RpcConsumer
+{
+
+    class Program
+    {
+        public static string Hostname = ""; //HostAdress
+        public static int Port = 8545; //port
+
+        public static string VirtualHost = ""; //HostName
+        public static string UserName = ""; //username
+        public static string Password = ""; //password
+
+        public static string QueueNameRPC = "RpcQueue";
+        public static string ExchangeNameRPC = "RpcQueueExchange";
+
+        static void Main(string[] args)
+        {
+            var connectionFactory = new ConnectionFactory
+            {
+                Port = Port,
+                HostName = Hostname,
+                UserName = UserName,
+                Password = Password,
+                VirtualHost = VirtualHost
+            };
+
+            using (var connection = connectionFactory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: QueueNameRPC, durable: false, exclusive: false, autoDelete: false, arguments: null);
+                channel.BasicQos(0, 1, false);
+
+                var consumer = new EventingBasicConsumer(channel);
+                channel.BasicConsume(queue: QueueNameRPC, autoAck: false, consumer: consumer);
+
+                Console.WriteLine(" [x] Awaiting RPC requests");
+
+                consumer.Received += (model, ea) =>
+                {
+                    string response = null;
+
+                    var body = ea.Body;
+                    var props = ea.BasicProperties;
+                    var replyProps = channel.CreateBasicProperties();
+                    replyProps.CorrelationId = props.CorrelationId;
+
+                    try
+                    {
+                        var message = Encoding.UTF8.GetString(body);
+                        int n = int.Parse(message);
+                        Console.WriteLine(" [.] fib({0})", message);
+                        Console.WriteLine("CorrelationID: " + replyProps.CorrelationId);
+                        response = fib(n).ToString() + " CorrelationID: " + replyProps.CorrelationId;
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(" [.] " + e.Message);
+                        response = "";
+                    }
+                    finally
+                    {
+                        var responseBytes = Encoding.UTF8.GetBytes(response);
+                        channel.BasicPublish(exchange: "", routingKey: props.ReplyTo,
+                          basicProperties: replyProps, body: responseBytes);
+                        channel.BasicAck(deliveryTag: ea.DeliveryTag,
+                          multiple: false);
+                    }
+                };
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+
+        private static int fib(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return n;
+            }
+
+            return fib(n - 1) + fib(n - 2);
+        }
+    }
+}
---
+++ DotnetCoreRabbitMQSamples/RpcPublisher/Program.cs
@@ -0,0 +1,97 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Concurrent;
+using System.Text;
+
+namespace RpcPublisher
+{
+    public class RpcClient
+    {
+        public static string Hostname = ""; //HostAdress
+        public static int Port = 8545; //port
+
+        public static string VirtualHost = ""; //HostName
+        public static string UserName = ""; //username
+        public static string Password = ""; //password
+
+        public static string QueueNameRPC = "RpcQueue";
+        public static string ExchangeNameRPC = "RpcQueueExchange";
+
+        private readonly IConnection connection;
+        private readonly IModel channel;
+        private readonly string replyQueueName;
+        private readonly EventingBasicConsumer consumer;
+        private readonly BlockingCollection<string> respQueue = new BlockingCollection<string>();
+        private readonly IBasicProperties props;
+
+        public RpcClient()
+        {
+            var connectionFactory = new ConnectionFactory
+            {
+                Port = Port,
+                HostName = Hostname,
+                UserName = UserName,
+                Password = Password,
+                VirtualHost = VirtualHost
+            };
+
+            connection = connectionFactory.CreateConnection();
+            channel = connection.CreateModel();
+            replyQueueName = channel.QueueDeclare().QueueName;
+            consumer = new EventingBasicConsumer(channel);
+
+            props = channel.CreateBasicProperties();
+            var correlationId = Guid.NewGuid().ToString();
+            props.CorrelationId = correlationId;
+            props.ReplyTo = replyQueueName;
+
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var response = Encoding.UTF8.GetString(body);
+                if (ea.BasicProperties.CorrelationId == correlationId)
+                {
+                    respQueue.Add(response);
+                }
+            };
+        }
+
+        public string Call(string message)
+        {
+            var messageBytes = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(
+                exchange: ExchangeNameRPC,
+                routingKey: QueueNameRPC,
+                basicProperties: props,
+                body: messageBytes);
+
+            channel.BasicConsume(
+                consumer: consumer,
+                queue: replyQueueName,
+                autoAck: true);
+
+            return respQueue.Take(); ;
+        }
+
+        public void Close()
+        {
+            connection.Close();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rpcClient = new RpcClient();
+
+            Console.WriteLine(" [x] Requesting fib(5)");
+            var response = rpcClient.Call("5");
+
+            Console.WriteLine(" [.] Got '{0}'", response);
+            rpcClient.Close();
+        }
+    }
+}
